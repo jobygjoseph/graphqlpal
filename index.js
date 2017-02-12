edf45384a4853134b61c305978bc37d67077829c
Joby Joseph <jobygjoseph@yahoo.com>
2017-02-11 21:55:18 -0500

    More scaffolding

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require('babel-register');
-
 require('dotenv').config({
   silent: true, // suppress warning if no .env file is present
 });
 
+const cp = require('child_process');
+
+if (process.NODE_ENV !== 'production') {
+  try {
+    const result = cp.execSync('git rev-parse --short HEAD', { encoding: 'utf-8' });
+    process.lastCommit = result.slice(0, 7);
+  } catch (e) {
+    //
+  }
+}
+
 //const startCluster = require('./lib/pal-utils/start-cluster');
-const server = require('./server');
+const server = require('./lib/server');
 
 if (process.env.NODE_ENV === 'development') {
   server();
